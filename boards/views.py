0ff8ff8285fb1345bbf53db9f855bb995f59b845
qwezxcasdqwe <krasn20161@mail.ru>
2025-06-19 02:50:41 +0300

    импорт задач по csv файлам

--- boards/views.py
+++ boards/views.py
@@ -5,6 +5,9 @@ from .serializers import BoardSerializer, ColumnSerializer, TaskSerializer,Comme
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdmin, IsManager, IsMember
+import csv
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 class BoardViewSet(viewsets.ModelViewSet):
   queryset = Board.objects.all()
@@ -41,6 +44,32 @@ class CommentViewSet(viewsets.ModelViewSet):
   def perform_create(self, serializer):
      serializer.save(author=self.request.user) #подставновка текущего автора при создании комментария
 
+def export_task_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="tasks.csv"'
     
+    writer = csv.writer(response)
+    writer.writerow(['ID', 'Tittle', 'Description', 'Deadline']) #заголовки
+    
+    for task in Task.objects.all():
+        writer.writerow([task.id, task.title, task.description, task.deadline])
+    
+    return response
+
+def import_tasks_view(request):
+    if request.method == 'POST':
+        csv_file = request.FILES['csv_file']
+        decode_file = csv_file.read().decode('utf-8').splitlines()
+        reader = csv.DictReader(decode_file)
+        
+        for row in reader:
+            Task.objects.create(
+                title = row['Title'],
+                description = row['Description'],
+                deadline=row['Deadline']
+            )
+        return redirect('task-list')
+    
+    return render(request,'import-task.html')    
 
 
